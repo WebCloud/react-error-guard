8fe970883535f97f700b09b01fbf619eb1d23f51
Vinicius Dallacqua <vinicius.dallacqua@klarna.com>
2018-03-13 10:41:50 +0100

    Use webpack to build the library

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+
+  output: {
+    library: 'react-error-guard',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'lib/'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+
+        options: {
+          presets: ['env'],
+        },
+      },
+    ],
+  },
+
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+      umd: 'react-dom',
+    },
+  },
+
+  plugins: [new UglifyJSPlugin()],
+};
