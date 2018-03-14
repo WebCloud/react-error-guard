fcfc1fc2d646e10041e4faa6e3123bedfb6bb3a5
Vinicius Dallacqua <vinicius.dallacqua@klarna.com>
2018-03-14 15:14:11 +0100

    Make the second line optional on the Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import React from 'react';
 import {darkGray} from '../styles';
 
@@ -18,8 +12,8 @@ function Footer(props) {
   return (
     <div style={footerStyle}>
       {props.line1}
-      <br />
-      {props.line2}
+      {props.line2 && (<br />)}
+      {props.line2 || null}
     </div>
   );
 }
